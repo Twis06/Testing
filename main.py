9239623a07be721d2754db4710eb5e60c630b2b2
Twis06 <lipy.5101@gmail.com>
2024-04-01 10:08:06 +0800

    V0

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import cv2
+from ultralytics import YOLO
+import numpy as np
+import torch
+
 
 cap = cv2.VideoCapture("app.mp4")
 
-ret, frame = cap.read()
+model = YOLO("yolov8m.pt")
 
-cv2.imshow("Img", frame)
-cv2.waitKey(0)
-m
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, device="mps")
+    result = results[0]
+    bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+    classes = np.array(result.boxes.cls.cpu(), dtype="int")
+
+    for cls, bbox in zip(classes, bboxes):
+        (x,y,x2,y2) = bbox
+
+        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
+        cv2.putText(frame, str(cls), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,225), 2)
+
+    cv2.imshow("Img", frame)
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
